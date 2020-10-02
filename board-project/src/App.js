9eed7af16fe0d6e743e280be46c595dbbcb7b7b3
jennethydyrova <jennethydyrovaa@gmail.com>
2020-10-02 11:33:22 +0300

    fixed bug in Board.js

--- board-project/src/App.js
+++ board-project/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Board from "./components/board/Board.js";
+// import Board from "./components/board/Board.js";
 import Boards from "./containers/boards/Boards";
 
 function App() {
   return (
     <div>
-      {/* <Board /> */}
       <Boards />
     </div>
   );
--- board-project/src/components/board/Board.js
+++ board-project/src/components/board/Board.js
@@ -3,29 +3,13 @@ import React, { useState, useEffect } from "react";
 import ItemForm from "../item/ItemForm";
 
 const Board = ({ oneBoard, boardsItems }) => {
-  // const [bla, setBla] = useState("");
-  console.log(oneBoard);
-  // console.log(boardsItems);
-  console.log(boardsItems);
-  // const renderItems = () => {
-  //   // boardsItems.map((item) => {
-  //   //   return <p>{item}</p>;
-  //   // });
-  //   // console.log(oneBoard);
-  // };
-  // useEffect(() => {
-  //   renderItems();
-  // }, [bla]);
-
   return (
     <div>
       <h3>{oneBoard}</h3>
-
-      {/* <h3>{oneBoard.title}</h3> */}
-      {/* {boardsItems.map((item) => {
+      {boardsItems.map((item) => {
         return <p>{item}</p>;
-      })} */}
-      {/* <ItemForm oneBoard={oneBoard} boardsItems={boardsItems} /> */}
+      })}
+      <ItemForm oneBoard={oneBoard} boardsItems={boardsItems} />
     </div>
   );
 };
